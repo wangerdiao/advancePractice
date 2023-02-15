e28124007c913c7daec9be8132fdb1aee36bcb36
王二雕 <2504204375@qq.com>
2023-02-15 17:48:17 +0800

    歌词滚动完成

--- 歌词滚动效果/js/index.js
+++ 歌词滚动效果/js/index.js
@@ -20,7 +20,6 @@ lrcList.forEach(item => {
     newLrcList.push(lrcObject)
 },
 )
-console.log(newLrcList);
 
 //计算时间的函数  参数time的形式为 '02:21.14'
 function parseTime(time) {
@@ -35,15 +34,16 @@ const doms = {
     container: document.querySelector('.container'),
 };
 
-//判断对应的时间对应的歌词显示高亮效果
-function showActiveLrc() {
+//判断对应的时间对应的歌词显示高亮效果,返回值为歌词的索引
+function findActiveIndex() {
     const currentTime = doms.audio.currentTime; //当前歌曲播放的时间
     const index = newLrcList.findIndex((item,index) => {
-        return currentTime<item.time  //返回符合当前判断的序号
+        return currentTime<item.time  //返回符合当前判断的序号,当没有找到是时会返回-1
     })
     return index-1
 }
-showActiveLrc()
+findActiveIndex()
+
 
 //创建歌词元素li
 function createElementLrcList() {
@@ -56,3 +56,40 @@ function createElementLrcList() {
     doms.ul.appendChild(frag)
 }
 createElementLrcList()
+
+// 容器高度
+var containerHeight = doms.container.clientHeight;
+console.log(containerHeight);
+// 每个 li 的高度
+var liHeight = doms.ul.children[0].clientHeight;
+// 最大偏移量
+var maxOffset = doms.ul.clientHeight - containerHeight;
+console.log(maxOffset);
+//添加高亮效果，以及设置ul的偏移量
+function showActive() {
+console.log(containerHeight);
+console.log(maxOffset);
+
+    let index = findActiveIndex() //找到当前时间对应的歌词
+    let offSetTop = liHeight*index + liHeight/2 + index *10 - containerHeight/2
+    console.log(offSetTop);
+    //绝对情况 歌词快播放完成时，页面不再滚动
+    if(offSetTop>maxOffset) {
+        offSetTop =  maxOffset
+    }
+    //绝对情况  找不到对应index返回负数
+    if(offSetTop<0) {
+        offSetTop = 0
+    }
+    doms.ul.style.transform = `translateY(-${offSetTop}px)` //向上滚动的距离
+     let li = doms.ul.querySelector('.active')
+    //排他思想
+    if(li) {
+        li.classList.remove('active')
+    }
+    li = doms.ul.children[index]
+    if(li) li.classList.add('active')
+}
+
+doms.audio.addEventListener('timeupdate',showActive)
+
